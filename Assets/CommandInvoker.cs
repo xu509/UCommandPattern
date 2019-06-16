fce3cc4d1277cb41d31dd7632a2bcd619f5c86ad
Alan <xu509@outlook.com>
2019-06-16 16:28:47 +0800

    11

---
+++ Assets/CommandInvoker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker : MonoBehaviour
+{
+    static Queue<ICommand> commandBuffer;
+
+    static List<ICommand> commandHistory;
+    static int counter;
+
+    private void Awake()
+    {
+        commandBuffer = new Queue<ICommand>();
+        commandHistory = new List<ICommand>();
+    }
+
+    public static void AddCommand(ICommand command) {
+        while (commandHistory.Count > counter) {
+            commandHistory.RemoveAt(counter);
+        }
+
+        commandBuffer.Enqueue(command);
+    }
+
+    private void Update()
+    {
+        if (commandBuffer.Count > 0)
+        {
+            ICommand c = commandBuffer.Dequeue();
+            c.Execute();
+
+            commandHistory.Add(c);
+            counter++;
+        }
+        else {
+            if (Input.GetKeyDown(KeyCode.Z)) {
+                if (counter > 0) {
+                    counter--;
+                    commandHistory[counter].Undo();
+                }
+            } else if (Input.GetKeyDown(KeyCode.R)){
+                if (counter < commandHistory.Count) {
+                    commandHistory[counter].Execute();
+                    counter++;
+                }
+            }
+        }
+    }
+
+}
